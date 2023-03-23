fa0cb463211e8bcbbfddf15d6f964e040e18b7ff
ahmed.baz <ahmed.baz@fawry.com>
2023-03-23 23:36:42 +0200

    use client-request servcie for the logging and discard the logging impl from currency-conversion-service

--- src/main/java/com/demo/skyros/repo/ClientRequestRepo.java
+++
@@ -1,9 +0,0 @@
-package com.demo.skyros.repo;
-
-import com.demo.skyros.entity.ClientRequestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRequestRepo extends JpaRepository<ClientRequestEntity, Long> {
-}
