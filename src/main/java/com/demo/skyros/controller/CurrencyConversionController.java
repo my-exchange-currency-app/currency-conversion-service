12dab853aad2ca966541d85e18938264feeeccd4
ahmed.baz <ahmed.baz@fawry.com>
2023-04-07 01:19:32 +0200

    update CurrencyExchangeProxy

--- src/main/java/com/demo/skyros/controller/CurrencyConversionController.java
+++ src/main/java/com/demo/skyros/controller/CurrencyConversionController.java
@@ -7,16 +7,18 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("currency")
 public class CurrencyConversionController {
 
     Logger log = Logger.getLogger(CurrencyConversionController.class.getSimpleName());
     @Autowired
     private CurrencyConversionService currencyConversionService;
 
-    @PostMapping("currency-conversion")
+    @PostMapping("conversion")
     public AppResponse exchangeCurrency(@RequestBody CurrencyExchangeVO vo) {
         return currencyConversionService.convertCurrency(vo);
     }
--- src/main/java/com/demo/skyros/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/demo/skyros/proxy/CurrencyExchangeProxy.java
@@ -3,13 +3,12 @@ package com.demo.skyros.proxy;
 import com.demo.skyros.vo.AppResponse;
 import com.demo.skyros.vo.CurrencyExchangeVO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "currency-exchange-service", path = "/currency")
 public interface CurrencyExchangeProxy {
 
-    @PostMapping("currency-exchange")
+    @PostMapping("exchange")
     AppResponse exchangeCurrency(@RequestBody CurrencyExchangeVO currencyExchangeVO);
 }
