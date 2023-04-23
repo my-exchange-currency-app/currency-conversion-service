8dd09bc150659929d450fa265e9666cfa3b5497e
ahmed.baz <ahmed.baz@fawry.com>
2023-04-23 16:48:12 +0200

    - update LoggerInterceptor with restTemplate - save request based on config

--- src/main/java/com/demo/skyros/filter/LoggerInterceptor.java
+++ src/main/java/com/demo/skyros/filter/LoggerInterceptor.java
@@ -1,12 +1,13 @@
 package com.demo.skyros.filter;
 
-import com.demo.skyros.proxy.LoggingProxy;
 import com.demo.skyros.vo.RequestVO;
 import com.google.common.io.CharStreams;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,11 +21,19 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
 
     private static final String REQUEST_ID = "REQUEST_ID";
     //@Autowired
-    private LoggingProxy loggingProxy;
+    //private LoggingProxy loggingProxy;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${log.url.add}")
+    private String url;
+    @Value("${log.save.request}")
+    private boolean saveRequest;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //saveClientRequest(request);
+        if (saveRequest) {
+            saveClientRequest(request);
+        }
         return super.preHandle(request, response, handler);
     }
 
@@ -35,11 +44,15 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
             requestVO.setRequestBody(body);
             requestVO.setTag("CONVERSION");
             requestVO.setRequestId(request.getHeader(REQUEST_ID));
-            getLoggingProxy().saveClientRequest(requestVO);
+            saveClientRequest(requestVO);
+            //getLoggingProxy().saveClientRequest(requestVO);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void saveClientRequest(RequestVO requestVO) {
+        restTemplate.postForObject(getUrl(), requestVO, void.class);
+    }
 
 }
